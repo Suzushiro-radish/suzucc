bfb062af1a33aa967d8986f26e8f50c976853e7a
suzushiro <d.takagi.radished@gmail.com>
2023-10-11 23:50:41 +0000

    Improve error message

--- suzucc.c
+++ suzucc.c
@@ -22,6 +22,8 @@ struct Token
     char *str;      // トークンの文字列
 };
 
+char *user_input;
+
 // 現在着目しているトークンを持つグローバル変数
 Token *token;
 
@@ -30,6 +32,21 @@ void error(char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// エラーの場所を報告する関数
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -50,7 +67,7 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
@@ -58,7 +75,7 @@ void expect(char op)
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -119,6 +136,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+
+    user_input = argv[1];
 
     token = tokenize(argv[1]);
 
