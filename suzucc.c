e5e2ce45ae58d9d751d7da20e89f243c1e4edd57
suzushiro <d.takagi.radished@gmail.com>
2023-10-15 13:00:31 +0000

    Stack machine

--- suzucc.c
+++ suzucc.c
@@ -224,6 +224,39 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+void gen(Node *node)
+{
+    if (node->kind == ND_NUM)
+    {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n"); // rhsをrdiにpop
+    printf("    pop rax\n"); // lhsをraxにpop
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("    add rax rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
